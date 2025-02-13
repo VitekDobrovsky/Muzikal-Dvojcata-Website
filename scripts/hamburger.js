dd1e4c03163980a9e0fcd0092dfffa4080ba1dfd
Vít Dobrovský <dobrovsky.vit@gmail.com>
2025-02-13 17:17:09 +0100

    hamburger menu bug fixes

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,18 +1,32 @@
 const hamburger = document.getElementById("hamburger");
 const page = document.getElementById("page");
 const nav = document.getElementById("navigation");
+const navLinks = document.querySelectorAll(".page__navigation-item");
 
 function changeHamburgerState() {
   hamburger.classList.toggle("active");
   nav.classList.toggle("active");
   page.classList.toggle("hidden");
 }
+
+function closeHamburger() {
+  hamburger.classList.remove("active");
+  nav.classList.remove("active");
+  page.classList.remove("hidden");
+}
+
 function handleResize() {
   if (window.innerWidth > 750) {
-    hamburger.classList.remove("active");
-    nav.classList.remove("active");
-    page.classList.remove("hidden");
+    closeHamburger();
+  } else if (window.innerWidth < 750) {
   }
 }
+
+navLinks.forEach((link) => {
+  if (window.innerWidth < 750) {
+    link.addEventListener("click", closeHamburger);
+    console.log("clicked");
+  }
+});
 
 window.addEventListener("resize", handleResize);
